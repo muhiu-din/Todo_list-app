932d140e89285d1d398f01419b28c6dafd0cbfab
Muhiu Din <muhiudin005@gmail.com>
2025-06-18 00:27:53 +0500

    completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-slate-600 p-4 text-white flex justify-between items-center'>
-    <div className="logo">
-        <h1 className='text-2xl font-bold'>Todo App</h1>
+    <nav className='bg-violet-500 p-4 text-white flex justify-between items-center'>
+    <div className="logo h-auto mx-8 ">
+       <img src="images/todox-icon.png" alt="logo" className="w-20  " />
     </div>
-    <ul className='flex gap-4'>
-        <li>Home</li>
-        <li>Your Tasks</li>
-        <li>More</li>
+    <ul className='flex gap-6 mx-8'>
+        <li className='cursor-pointer font-semibold hover:font-bold transition-all duration-400  flex justify-center'>Your Tasks</li>
+        <li className='cursor-pointer font-semibold hover:font-bold transition-all duration-400 w-[3vw] flex justify-center'>More</li>
     </ul>
     </nav>
   )
